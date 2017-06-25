817d54868762561d3d80a69f3c8b077442842867
Alex Devero <deveroalex@gmail.com>
2017-06-25 16:05:40 +0200

    Add alpha version of timer

--- src/components/session.js
+++ src/components/session.js
@@ -8,8 +8,54 @@ export default class Session extends Component {
 	constructor(props) {
 		super(props);
 
+    this.state = {
+      time: {},
+      seconds: 5
+    };
+    this.timer = 0;
+    this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
+
 		this.store = this.props.store;
 	}
+
+  secondsToTime(secs){
+    let hours = Math.floor(secs / (60 * 60));
+
+    let divisor_for_minutes = secs % (60 * 60);
+    let minutes = Math.floor(divisor_for_minutes / 60);
+
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    let seconds = Math.ceil(divisor_for_seconds);
+
+    let obj = {
+      'h': hours,
+      'm': minutes,
+      's': seconds
+    };
+    return obj;
+  }
+
+  startTimer() {
+    if (this.timer === 0) {
+      this.timer = setInterval(this.countDown, 1000);
+    }
+  }
+
+  countDown() {
+    // Remove one second, set state so a re-render happens.
+    let seconds = this.state.seconds - 1;
+
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds,
+    });
+
+    // Check if we're at zero.
+    if (this.state.seconds === 0) {
+      clearInterval(this.timer);
+    }
+  }
 
 	render() {
 		const store = this.store;
@@ -19,6 +65,12 @@ export default class Session extends Component {
 				<main>
   				<div className='home__hero'>
 						<h4>Set the duration of your session.</h4>
+
+            <div>m: {this.state.time.m} s: {this.state.time.s}</div>
+
+            <div>
+              <button onClick={this.startTimer}>Start</button>
+            </div>
   				</div>
 				</main>
 			</div>
