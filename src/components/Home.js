e70eb1ffce27abee8a31383004684e1d65aecf66
Alex Devero <deveroalex@gmail.com>
2017-06-27 10:14:15 +0200

    Change classes

--- src/components/Home.js
+++ src/components/Home.js
@@ -27,7 +27,7 @@ export default class Home extends Component {
   					<div className='hero__content'>
   						<h4>A simple meditation timer to help you practice meditation.</h4>
 
-              <Link to='/session'>Give it a try and start your first session!</Link>
+              <Link className='home-cta' to='/session'>Give it a try and start your first session!</Link>
   					</div>
   				</div>
 				</main>
