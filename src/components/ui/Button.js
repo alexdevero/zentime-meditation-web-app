335bc31e5ba179752459b99fb1fd7dc6cbfdaf12
Alex Devero <deveroalex@gmail.com>
2017-06-27 10:11:43 +0200

    Add button variants, change button comp.

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 
 const Button = ({ ...props }) => {
-	return <a className='button' onClick={props.onClick}>{props.title}</a>;
+	return <a className={props.className} onClick={props.onClick}>{props.title}</a>;
 };
 
 export default Button;
