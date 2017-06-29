2edeb211d1f4ee9906ed38a8b1294d82768868c5
Alex Devero <deveroalex@gmail.com>
2017-06-29 16:53:42 +0200

    Implement spread for input props

--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 
 const Input = ({ ...props }) => {
-	return <input value={props.value} className={props.className} onChange={props.onChange} name={props.name} type={props.type} placeholder={props.placeholder} />;
+	return <input {...props} />;
 };
 
 export default Input;
