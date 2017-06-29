5fb63bb3cfcc806fb2fc87d18bd58b775547f934
Alex Devero <deveroalex@gmail.com>
2017-06-29 12:50:30 +0200

    Add user-based timer

--- src/components/session.js
+++ src/components/session.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import { Link } from 'react-router-dom';
 
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+import classNames from 'classnames';
+
 @inject('store')
 @observer
 export default class Session extends Component {
@@ -37,6 +42,11 @@ export default class Session extends Component {
     if (this.timer === 0) {
       this.store.appState.timerIsRunning = true;
       this.timer = setInterval(this.countDown, 1000);
+    } else {
+      this.store.appState.timerIsRunning = false;
+      this.store.appState.timerIsStopped = true;
+
+      clearInterval(this.timer);
     }
   }
 
@@ -64,23 +74,18 @@ export default class Session extends Component {
       // Check if there are any minutes remaining
       if (this.store.appState.timerMinutes > 0) {
         this.decreaseMinutes();
-        // this.store.appState.timerMinutes = this.store.appState.timerMinutes - 1;
 
-        // this.store.appState.timerSeconds = this.store.appState.timerSeconds + 60;
+        this.countDown();
       } else {
         // Check if there are any hours remaining
         if (this.store.appState.timerHours > 0) {
           this.decreaseHours();
-          // this.store.appState.timerHours = this.store.appState.timerHours - 1; // Decrease the hours by one
 
-          // this.store.appState.timerMinutes = this.store.appState.timerMinutes + 60; // Add 60 minutes to minutes
-
+          this.countDown();
           if (this.store.appState.timerMinutes > 0) {
             this.decreaseMinutes();
-            // this.store.appState.timerMinutes = this.store.appState.timerMinutes - 1;
 
-            // this.store.appState.timerSeconds = this.store.appState.timerSeconds + 60;
-            // this.countDown();
+            this.countDown();
           }
         }
       }
@@ -90,9 +95,33 @@ export default class Session extends Component {
 
         this.store.appState.timerIsFinished = true;
         this.store.appState.timerIsRunning = false;
+        this.timer = 0;
 
         console.log('timer is finished: ' + this.store.appState.timerIsFinished);
       }
+    }
+  }
+
+  updateTimer(event) {
+    let timerType = event.target.name;
+    let userInput = event.target.value;
+
+    this.store.appState.timerIsFinished = false;
+    this.store.appState.timerIsRunning = false;
+    this.store.appState.timerIsStopped = false;
+
+    if (timerType.indexOf('hours') !== -1) {
+      this.store.appState.timerHours = userInput;
+    } else if (event.target.name.indexOf('minutes') !== -1) {
+      this.store.appState.timerMinutes = userInput;
+    } else if (event.target.name.indexOf('seconds') !== -1)  {
+      this.store.appState.timerSeconds = userInput;
+    }
+  }
+
+  formatTimerValues(value) {
+    if (value.toString().length === 1) {
+      return ('0' + value);
     }
   }
 
@@ -105,10 +134,22 @@ export default class Session extends Component {
   				<div className='home__hero'>
 						<h4>Set the duration of your session.</h4>
 
-            <div>h: {this.store.appState.timerHours} m: {this.store.appState.timerMinutes} s: {this.store.appState.timerSeconds}</div>
+            <div className={classNames('session-timer form-inline', this.store.appState.timerIsRunning && 'hidden')}>
+              <Input value={/*this.formatTimerValues(*/this.store.appState.timerHours/*)*/} onChange={this.updateTimer.bind(this)} className='form-control' name='timer-hours' type='text' />
+
+              <Input value={/*this.formatTimerValues(*/this.store.appState.timerMinutes/*)*/} onChange={this.updateTimer.bind(this)} className='form-control' name='timer-minutes' type='text' />
+
+              <Input value={/*this.formatTimerValues(*/this.store.appState.timerSeconds/*)*/} onChange={this.updateTimer.bind(this)} className='form-control' name='timer-seconds' type='text' />
+            </div>
+
+            <div className={classNames('session-countdown', !this.store.appState.timerIsRunning && 'hidden')}>
+              {this.store.appState.timerHours} h : {this.store.appState.timerMinutes} m : {this.store.appState.timerSeconds} s
+            </div>
 
             <div>
-              <button onClick={this.startTimer}>Start</button>
+              {!this.store.appState.timerIsStopped && <Button className={classNames('btn', !this.store.appState.timerIsRunning ? 'btn btn--secondary' : 'btn btn--danger')} onClick={this.startTimer} title={!this.store.appState.timerIsRunning? 'Start session' : 'Stop session'} />}
+
+              {this.store.appState.timerIsStopped && <Button className='btn btn--primary' onClick={this.startTimer} title='Restart Timer' />}
             </div>
   				</div>
 				</main>
