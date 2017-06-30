b4805bd78849a387a415f23b77ab464939cb34fb
Alex Devero <deveroalex@gmail.com>
2017-06-30 14:04:49 +0200

    Add rule for mp3 files

--- webpack.config.js
+++ webpack.config.js
@@ -84,6 +84,10 @@ module.exports = {
         ]
       },
       {
+        test: /\.mp3$/,
+        use: 'file-loader'
+      },
+      {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         use: 'url-loader?limit=10000&mimetype=application/font-woff'
       },
