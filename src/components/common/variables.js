26c42c8897b8686db7a2055eff90b791e63fb8d7
Alex Devero <deveroalex@gmail.com>
2017-11-26 08:54:38 +0100

    Create variables for styled-components

---
+++ src/components/common/variables.js
@@ -0,0 +1,23 @@
+// Variables
+
+// Colors
+export const colorBlue = '#03a9f4'
+export const colorBlueLight = '#4fc3f7'
+export const colorGreen = '#00eb79'
+export const colorGreenLight = '#00ff83'
+export const colorPink = '#b24592' // fallback for gradient
+export const colorRed = '#f44336'
+export const colorRedLight = '#f77066'
+
+// Gradients
+export const gradientPurple = 'radial-gradient(ellipse farthest-corner at top left,#7b16ff 0%,#4400cc 100%)'
+export const gradientPurpleFallback = '#7b16ff'
+
+export const gradientRed = 'radial-gradient(ellipse farthest-corner at top left, #ff6a00 0%, #ee0979 100%)'
+export const gradientRedFallback = '#ee0979'
+
+// Miscelaneous
+export const radius = '35px'
+
+export const transitionDuration = '.25s'
+export const transitionTiming = 'ease-in-out'
